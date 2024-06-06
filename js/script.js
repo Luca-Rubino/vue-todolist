033e2a71978978801f8c8b27bdf9741fb42bb491
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-06 11:19:39 +0200

    Scrivere array todoList con all'interno la lista degli elementi e la funzione per sbarraregli elementi della lista

--- js/script.js
+++ js/script.js
@@ -1,2 +1,38 @@
 console.log('Vue-todolist')
 
+const { createApp } = Vue
+
+createApp({
+data() {
+    return {
+        titolo: "Vue-todolist",
+        todoList: [
+            {
+                content: "Lanciafiamme",
+                done: false
+            },
+            {
+                content: "Patate al forno",
+                done: false
+            },
+            {
+                content: "Hamburger vegano",
+                done: false
+            },
+            {
+                content: "Parrucca bionda",
+                done: false
+            },
+            {
+                content: "Shampoo per baffi",
+                done: false
+            },
+        ]
+    }
+},
+methods: {
+    sbarratoInElenco : function(itemIndex) {
+        this.todoList[itemIndex].done = !this.todoList[itemIndex].done;
+    }
+}
+}).mount('#app')
